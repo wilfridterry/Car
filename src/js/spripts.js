dab79309e949b3204c89ed0e0afa2e122d6338e8
Stannis <zalevschi1998@gmail.com>
2022-05-24 23:59:04 +0300

    Worked with responsive

--- src/js/spripts.js
+++ src/js/spripts.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const element = document.querySelector('.hamburger');
-
+    const menu = document.querySelector('.menu');
+     
     element.addEventListener('click', () => {
         element.classList.toggle('hamburger_active');
-        document.querySelector('.menu').classList.toggle('menu_active');
+        menu.classList.toggle('menu_active');
     });
 });
